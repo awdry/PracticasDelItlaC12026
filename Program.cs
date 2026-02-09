f0d56f4be5c255eceee7ef022877c80bf2af7550
Awdry Nolasco <nolascoawdry@gmail.com>
2026-02-08 20:40:45 -0400

    clarar variable de los diferentes tipos, asignarles valor e imprimir el valor.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Declarar variable de los diferentes tipos
+//asignarles valor e imprimir el valor.
+
+string name = "Awdry";
+int age = 19;
+double height = 1.64;
+decimal weight = 9.8m;
+bool impretty = true;
+
+Console.WriteLine($"nombre: {name}");
+Console.WriteLine($"edad: {age}");
+Console.WriteLine($"altura: {height}");
+Console.WriteLine($"Peso: {weight}");
+Console.WriteLine($"¿Soy bonita? {impretty}");
