43bcb52eec78acdab226e836d888cd0d2363e578
Awdry Nolasco <nolascoawdry@gmail.com>
2026-02-09 22:41:32 -0400

    Declara un entero, incrementarlo, decrementarlo, hacer operaciones con el.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿//Declarar variable de los diferentes tipos
 //asignarles valor e imprimir el valor.
 
+using System.Data;
+
 string name = "Awdry";
 int age = 19;
 double height = 1.64;
@@ -17,4 +19,22 @@ Console.WriteLine($"¿Soy bonita? {impretty}");
 
 const double pi = 3.14;
 Console.WriteLine($"El valor de pi es: {pi}");
-pi = 3.14159; 
+//pi = 3.14159; 
+
+//Declara un entero,
+//incrementarlo, decrementarlo,
+//hacer operaciones con el.
+
+int number = 23;
+number++;
+Console.WriteLine($"Numero Incrementando: {number}");
+number--;
+Console.WriteLine($"Numero Decrementando {number}");
+int sum = number + 20;
+int difference = number - 5;
+int product = number * 2;
+int quotient = number / 3;
+Console.WriteLine($"Suma= {sum}");
+Console.WriteLine($"Resta= {difference}");
+Console.WriteLine($"Multiplicacion= {product}");
+Console.WriteLine($"Division= {quotient}");
